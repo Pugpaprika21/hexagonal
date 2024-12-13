b107131c593e6caaf3e93b9b8bddb9694336509f
__Pug__ <59162541+Pugpaprika21@users.noreply.github.com>
2024-12-13 04:44:18 -0800

    add users.IUserHandler

--- app/internal/adapter/router/v1.go
+++ app/internal/adapter/router/v1.go
@@ -1,5 +1,23 @@
 package router
 
-func (r *appRouter) v1() {
+import (
+	"net/http"
 
+	"github.com/labstack/echo/v4"
+)
+
+func (r *appRouter) v1() {
+	api := r.server.Group("/api")
+
+	v1 := api.Group("/v1")
+	{
+		users := v1.Group("/users")
+		{
+			users.POST("/get", r.handler.Users.GetUsers)
+		}
+
+		v1.GET("/heat_check", func(c echo.Context) error {
+			return c.String(http.StatusOK, "Is OK!")
+		})
+	}
 }
