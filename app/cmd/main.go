b4fb460545c33dc5fe711d3c9df86bcf4f19a380
__Pug__ <59162541+Pugpaprika21@users.noreply.github.com>
2024-12-13 19:41:02 -0800

    update main.go

--- app/cmd/main.go
+++ app/cmd/main.go
@@ -2,39 +2,54 @@ package main
 
 import (
 	"log"
+	"os"
+	"os/signal"
 	"pugpaprika/app/internal/adapter/handler"
 	"pugpaprika/app/internal/adapter/repository"
 	"pugpaprika/app/internal/adapter/router"
 	"pugpaprika/app/internal/core/service"
 	"pugpaprika/app/pkg/config"
 	"pugpaprika/app/pkg/factory"
+	"syscall"
 )
 
 func main() {
-	err := config.Loadenvironment()
-	if err != nil {
-		log.Fatalf("error loading environment config : %s", err.Error())
+	if err := config.Loadenvironment(); err != nil {
+		log.Fatalf("Error loading environment config: %s", err)
 	}
 
 	connector, err := factory.NewDatabase()
 	if err != nil {
-		log.Fatalf("error selecting database: %s", err.Error())
+		log.Fatalf("Error selecting database: %s", err)
 	}
 
 	db, err := connector.Connect()
 	if err != nil {
-		log.Fatalf("error establishing database connection: %s", err.Error())
+		log.Fatalf("Error establishing database connection: %s", err)
 	}
 
 	defer func() {
 		if err := connector.Close(); err != nil {
-			log.Fatalf("error closing database connection: %s", err.Error())
+			log.Printf("Error closing database connection: %s", err)
 		}
 	}()
 
 	repos := repository.NewRepository(db)
 	servs := service.NewService(repos)
 	handlers := handler.NewHandler(servs)
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		sig := <-signalChan
+		log.Printf("Received signal: %s. Shutting down...", sig)
+		if err := connector.Close(); err != nil {
+			log.Printf("Error closing database connection during shutdown: %s", err)
+		}
+
+		os.Exit(0)
+	}()
 
 	router.NewRouter(handlers).Start()
 }
