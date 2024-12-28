9f7ccbf63cb2547c901fd5b2fcb8a774e6e03c7c
__Pug__ <59162541+Pugpaprika21@users.noreply.github.com>
2024-12-28 05:23:54 -0800

    add factory.NewDatabase(os.Getenv("DB_DRIVER"))

--- app/cmd/main.go
+++ app/cmd/main.go
@@ -18,7 +18,7 @@ func main() {
 		log.Fatalf("Error loading environment config: %s", err)
 	}
 
-	connector, err := factory.NewDatabase()
+	connector, err := factory.NewDatabase(os.Getenv("DB_DRIVER"))
 	if err != nil {
 		log.Fatalf("Error selecting database: %s", err)
 	}
