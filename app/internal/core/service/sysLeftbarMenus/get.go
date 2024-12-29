79dac5a29e2d352414f33c9f8ac17b6afef86f9c
__Pug__ <59162541+Pugpaprika21@users.noreply.github.com>
2024-12-29 06:20:07 -0800

    add GetMainManus

--- app/internal/core/service/sysLeftbarMenus/get.go
+++ app/internal/core/service/sysLeftbarMenus/get.go
@@ -14,50 +14,50 @@ func (s *sysLeftBarMenusService) GetMainManus(ctx context.Context, req request.G
 
 	sql.Stmt = `
 	select 
-		m1.id as menu_id,
-		m1.name as menu_name,
-		m1.url as menu_url,
-		m1.icon as menu_icon,
-		m1.tooltip as menu_tooltip,
-		m1.position as menu_position,
-		m1.is_active as menu_is_active,
-		m2.id as sub_menu_id,
-		m2.name as sub_menu_name,
-		m2.url as sub_menu_url,
-		m2.icon as sub_menu_icon,
-		m2.tooltip as sub_menu_tooltip,
-		m2.position as sub_menu_position,
-		m2.is_active as sub_menu_is_active,
-		m1.user_id,
-    	m1.role_id
+		p.id as menu_id,
+		p.name as menu_name,
+		p.url as menu_url,
+		p.icon as menu_icon,
+		p.tooltip as menu_tooltip,
+		p.position as menu_position,
+		p.is_active as menu_is_active,
+		s.id as sub_menu_id,
+		s.name as sub_menu_name,
+		s.url as sub_menu_url,
+		s.icon as sub_menu_icon,
+		s.tooltip as sub_menu_tooltip,
+		s.position as sub_menu_position,
+		s.is_active as sub_menu_is_active,
+		p.user_id,
+    	p.role_id
 	from 
-		sys_leftbar_menus m1
+		sys_leftbar_menus p
 	left join 
-		sys_leftbar_menus m2 on m1.id = m2.parent_id 
+		sys_leftbar_menus s on p.id = s.parent_id 
 	`
 	if req.MenuID != nil && *req.MenuID != 0 {
-		sql.WhereClause = append(sql.WhereClause, "menu_id = ?")
+		sql.WhereClause = append(sql.WhereClause, "p.menu_id = ?")
 		sql.Args = append(sql.Args, req.MenuID)
 	}
 
 	if req.UserID != nil && *req.UserID != 0 {
-		sql.WhereClause = append(sql.WhereClause, "user_id = ?")
+		sql.WhereClause = append(sql.WhereClause, "p.user_id = ?")
 		sql.Args = append(sql.Args, req.UserID)
 	}
 
 	if req.RoleID != nil && *req.RoleID != 0 {
-		sql.WhereClause = append(sql.WhereClause, "role_id = ?")
+		sql.WhereClause = append(sql.WhereClause, "p.role_id = ?")
 		sql.Args = append(sql.Args, req.RoleID)
 	}
 
-	sql.WhereClause = append(sql.WhereClause, "m1.is_active = ?")
+	sql.WhereClause = append(sql.WhereClause, "p.is_active = ?")
 	sql.Args = append(sql.Args, true)
 
 	if len(sql.WhereClause) > 0 {
 		sql.Stmt += " Where " + strings.Join(sql.WhereClause, " and ")
 	}
 
-	sql.Stmt += ` order by m1.position asc, m2.position asc;`
+	sql.Stmt += ` order by p.position asc, s.position asc;`
 
 	rows, err := s.repos.GetMainManus(ctx, sql)
 	if err != nil {
